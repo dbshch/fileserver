3119003aaa592c5d005c9bcd32ec47c40cc764e5
mr-cloud <zhangxin1992pm@gmail.com>
2018-01-31 15:20:09 +0800

    Added toggle for CORS Support.

--- src/main/java/uni/akilis/file_server/config/AppConfig.java
+++ src/main/java/uni/akilis/file_server/config/AppConfig.java
@@ -1,14 +1,16 @@
 package uni.akilis.file_server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import uni.akilis.file_server.filter.TokenInterceptor;
 import uni.akilis.file_server.util.Consts;
 
 /**
- * Validating the incoming request.
+ *
  * @author leo
  */
 @Configuration
@@ -16,9 +18,26 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     @Autowired
     private TokenInterceptor tokenInterceptor;
 
+    /**
+     * Validating the incoming request.
+     * @param registry
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
                 .addPathPatterns(Consts.UP_DOWN_PATH + "**");
+    }
+
+    @Value("${CORS_TOGGLE}")
+    private boolean corsToggle = false;
+
+    /**
+     * Support CORS for upload and download APIs.
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        if (corsToggle)
+            registry.addMapping(Consts.UP_DOWN_PATH + "**");
     }
 }
